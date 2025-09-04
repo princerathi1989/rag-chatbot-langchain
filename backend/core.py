e7a85ab8014c1e7e1e280ddf15548b516a80d6fc
Prince Chaudhary <princerathi1989@gmail.com>
2025-09-04 09:05:17 +0530

    Retrieval + Augmentation + Generation

---
+++ backend/core.py
@@ -0,0 +1,40 @@
+import os
+from langchain.chains.retrieval import create_retrieval_chain
+# from langchain_community.llms.ollama import Ollama
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_pinecone import PineconeVectorStore
+
+from dotenv import load_dotenv
+
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+
+load_dotenv()
+
+def run_llm(query: str):
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    docsearch = PineconeVectorStore(index_name = os.getenv('PINECONE_INDEX_NAME'), embedding = embeddings)
+    chat = ChatOpenAI(verbose=True, temperature=0)
+    # chat = Ollama(model="llama3")
+
+    retrieval_qa_chat_prompt: PromptTemplate = hub.pull(
+        "langchain-ai/retrieval-qa-chat",
+    )
+    stuff_documents_chain = create_stuff_documents_chain(
+        chat, retrieval_qa_chat_prompt
+    )
+
+    qa = create_retrieval_chain(
+        retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain
+    )
+    result = qa.invoke(input={"input": query})
+    return result
+
+if __name__ == "__main__":
+    # res = run_llm(query="What is a LangChain Chain?")
+    res = run_llm(query="What is Task Decomposition?")
+    # res = run_llm(query="How to make Pizza?")
+
+    print(res["answer"])
+
